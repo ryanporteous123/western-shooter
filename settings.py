a25fd084023fa73fa9c12c916fcba4d433bea9e7
RYANPORTEOUS123 <RYAN.PORTEOUS123@icloud.com>
2023-06-04 10:24:30 +0100

    western shooter

---
+++ settings.py
@@ -0,0 +1,7 @@
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+
+PATHS = {
+    'player': 'p1_setup/graphics/player',
+    'coffin': 'p1_setup/graphics/monster/coffin',
+    'cactus': 'p1_setup/graphics/monster/cactus'
+}
